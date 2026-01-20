3cb05057ea5b3ead60420d161055722f7fa67df4
Mukola <vmv.vynnychuk98@gmail.com>
2026-01-20 17:59:46 +0200

    dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,50 @@
 import { useState } from "react"
-import { DndContext, useDraggable } from "@dnd-kit/core"
 
-type Draggables = {
+import { DndContext, useDraggable, useDroppable, type DragEndEvent } from "@dnd-kit/core"
+
+type Draggable = {
   id: string
   src: string
+  dz?: string
 }
 
 //«Цей масив МОЖЕ містити ТІЛЬКИ обʼєкти типу Draggables»
-const defaultDraggables: Draggables[] = [
-  { id: crypto.randomUUID(), src: "dreamland.png" },
-  { id: crypto.randomUUID(), src: "kill_bill.png" },
-  { id: crypto.randomUUID(), src: "kingdom_of_heaven.png" },
-  { id: crypto.randomUUID(), src: "mulholland_drive.png" },
-  { id: crypto.randomUUID(), src: "mystic_river.png" },
-  { id: crypto.randomUUID(), src: "pulp_fiction.png" },
-  { id: crypto.randomUUID(), src: "seven.png" },
-  { id: crypto.randomUUID(), src: "shutter_island.png" },
-  { id: crypto.randomUUID(), src: "The Neon Demon.png" },
-  { id: crypto.randomUUID(), src: "the_god_father.png" },
-  { id: crypto.randomUUID(), src: "the_lord_of_the_rings_the_fellowship_of_the_ring.png" },
-  { id: crypto.randomUUID(), src: "the_lord_of_the_rings_the_return_of_the_rings.png" },
-  { id: crypto.randomUUID(), src: "the_lord_of_the_rings_the_two_towers.png" },
-  { id: crypto.randomUUID(), src: "the_silence_of_lambs.png" },
-  { id: crypto.randomUUID(), src: "Troy.png" },
-  { id: crypto.randomUUID(), src: "watchman.png" },
+const defaultDraggables: Draggable[] = [
+  { id: crypto.randomUUID(), src: "dreamland.png", dz: undefined },
+  { id: crypto.randomUUID(), src: "kill_bill.png", dz: undefined },
+  { id: crypto.randomUUID(), src: "kingdom_of_heaven.png", dz: undefined },
+  { id: crypto.randomUUID(), src: "mulholland_drive.png", dz: undefined },
+  { id: crypto.randomUUID(), src: "mystic_river.png", dz: undefined },
+  { id: crypto.randomUUID(), src: "pulp_fiction.png", dz: undefined },
+  { id: crypto.randomUUID(), src: "seven.png", dz: undefined },
+  { id: crypto.randomUUID(), src: "shutter_island.png", dz: undefined },
+  { id: crypto.randomUUID(), src: "The Neon Demon.png", dz: undefined },
+  { id: crypto.randomUUID(), src: "the_god_father.png", dz: undefined },
+  { id: crypto.randomUUID(), src: "the_lord_of_the_rings_the_fellowship_of_the_ring.png", dz: undefined },
+  { id: crypto.randomUUID(), src: "the_lord_of_the_rings_the_return_of_the_rings.png", dz: undefined },
+  { id: crypto.randomUUID(), src: "the_lord_of_the_rings_the_two_towers.png", dz: undefined },
+  { id: crypto.randomUUID(), src: "the_silence_of_lambs.png", dz: undefined },
+  { id: crypto.randomUUID(), src: "Troy.png", dz: undefined },
+  { id: crypto.randomUUID(), src: "watchman.png", dz: undefined },
 ]
 
 export default function App() {
-  const [draggables, ] = useState<Draggables[]>(defaultDraggables)
+  const [draggables, setDraggables] = useState<Draggable[]>(defaultDraggables)
+
+  const handleDragEnd = (e: DragEndEvent) => {
+    console.log(e);
+    if (!e.over) return;
+    const dropZoneId = e.over.id as string
+    const activeDraggableId = e.active.id as string
+    setDraggables(prev => prev.map(draggable => draggable.id !== activeDraggableId ? draggable : { ...draggable, dz: dropZoneId }))
+  }
 
   return (
-    <div className="w-screen h-screen flex jusrify-center items-center">
-      <DndContext>
-        <div className="flex gap-2">
-          {draggables.map(draggable => (
+    <div className="w-screen h-screen flex flex-col gap-16 jusrify-center items-center">
+      <DndContext onDragEnd={handleDragEnd}>
+        <DropZone draggables={draggables} />
+        <div className="flex gap-1">
+          {draggables.filter(draggable => !draggable.dz).map(draggable => (
             <Draggable key={draggable.id} draggable={draggable} />
           ))}
         </div>
@@ -42,7 +53,28 @@ export default function App() {
   )
 }
 
-function Draggable({ draggable }: { draggable: Draggables }) {  /*{ цей код перевіряє props і гарантує правильні типи} */
+function DropZone({ draggables }: { draggables: Draggable[] }) {
+  const { setNodeRef, isOver } = useDroppable({ id: `dropZone` })
+
+  const style = {
+    backgroundColor: isOver ? 'green' : undefined,
+  }
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      className="border border-white bg-[#333] h-34 w-full flex gap-4"
+    >
+      {draggables
+        .filter(draggable => draggable.dz)
+        .map(draggable => (
+          <Draggable key={draggable.id} draggable={draggable} />
+        ))}
+    </div>
+  )
+}
+
+function Draggable({ draggable }: { draggable: Draggable }) {  /*{ цей код перевіряє props і гарантує правильні типи} */
   const { id, src } = draggable
 
   const { setNodeRef, listeners, attributes, transform } = useDraggable({ id })
@@ -67,4 +99,6 @@ function Draggable({ draggable }: { draggable: Draggables }) {  /*{ цей ко�
       />
     </button>
   )
-} 
+}
+
+
